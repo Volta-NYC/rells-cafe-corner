fad85f74eeacb2b9d19bf1ce67161c6a7260f143
henry mac <henryozhao@gmail.com>
2026-05-06 23:13:17 -0400

    feat: import Image component from Next.js for future image integration

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { Plus } from "lucide-react";
 import { useState } from "react";
 import { makeItemId, parsePrice, useCart } from "@/lib/cart";
